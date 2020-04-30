0d10fafd9d08a2d468a0156c5697aa6e5ad962c1
Hernán Torres <hernantorres.07@gmail.com>
2020-04-30 19:13:22 -0300

    Layout

--- frontend/src/components/layout/Landing.js
+++ frontend/src/components/layout/Landing.js
@@ -1,27 +1,18 @@
 import React from 'react';
-import { Link, Redirect } from 'react-router-dom';
 
-const Landing = ({ isAuthenticated }) => {
-  if (isAuthenticated) {
-    return <Redirect to='/dashboard' />;
-  }
-
-  return (
-    <section className='landing'>
-      <div className='dark-overlay'>
-        <div className='landing-inner'>
-          <h1 className='x-large'>Serendipity</h1>
+const Landing = () => {
+    return (
+      <section class="landing">
+      <div class="dark-overlay">
+        <div class="landing-inner">
+          <h1 class="x-large">Serendipity</h1>
           <h3 className='lead'>Connected to the world!</h3>
-          <p className='lead'>
+          <p class="lead">
           Create your profile, and start being connected
           </p>
-          <div className='buttons'>
-            <Link to='/register' className='btn btn-primary'>
-              Sign Up
-            </Link>
-            <Link to='/login' className='btn btn-light'>
-              Login
-            </Link>
+          <div class="buttons">
+            <a href="register.html" class="btn btn-primary">Sign Up</a>
+            <a href="login.html" class="btn btn-light">Login</a>
           </div>
         </div>
       </div>
