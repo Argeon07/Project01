be8aa960c5ac31b1631394214a97e1db1316fd10
Hernán Torres <hernantorres.07@gmail.com>
2020-05-07 19:05:45 -0300

    DeleteAccount

--- frontend/src/actions/profile.js
+++ frontend/src/actions/profile.js
@@ -65,3 +65,22 @@ export const createProfile = (formData, history, edit = false) => async (
     });
   }
 };
+
+// Delete account & profile
+export const deleteAccount = () => async dispatch => {
+  if (window.confirm('Are you sure? This can NOT be undone!')) {
+    try {
+      await axios.delete('/api/profile');
+
+      dispatch({ type: CLEAR_PROFILE });
+      dispatch({ type: ACCOUNT_DELETED });
+
+      dispatch(setAlert('Your account has been permanently deleted'));
+    } catch (err) {
+      dispatch({
+        type: PROFILE_ERROR,
+        payload: { msg: err.response.statusText, status: err.response.status }
+      });
+    }
+  }
+};
